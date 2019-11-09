73c0a61fb15ff99de94db4ab404cdd5adde31be4
linaverde <p.koroleva1102@gmail,com>
2019-11-09 11:51:50 +1000

    New version + js file

---
+++ JS/bubblesort.js
@@ -0,0 +1,12 @@
+function bubblesort() {
+    let num = [1, 192, 16, 7, 538, 300, 42, 66, 15, 14];
+    console.log(num);
+    for (let i = 0; i < 10; i++){
+        for (let j = 1; j < i; j++){
+            if (num [j] > num [j-1]){
+                [num[j], num[j-1]] = [num[j-1], num[j]];
+            }
+        }
+    }
+    console.log(num);
+}
